bc2452b381ebf2773079bf80cbc5fb615197ed9f
miguelfajardoc <775@holbertonschool.com>
2020-04-22 18:04:22 -0500

    [add] recursive isEven number

---
+++ Chapter3-Functions/recursion.js
@@ -0,0 +1,18 @@
+function isEven(number)
+{
+    if (number < 0)
+    {
+        number *= -1;
+    }
+    if (number === 0)
+    {
+        return true;
+    } else if (number === 1)
+    {
+        return false;
+    } else
+    {
+        return isEven(number - 2);
+    }
+}
+console.log(isEven(-1));
